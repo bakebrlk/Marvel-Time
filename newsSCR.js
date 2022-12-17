22604edbb36c6e79f607219335c38da7ce977848
Yeldos Zharkynbekov <210103269@gmail.com>
2022-12-17 19:29:45 +0600

    gl

---
+++ newsSCR.js
@@ -0,0 +1,14 @@
+
+function bigger(zat){
+    zat.style.fontSize = "30px";
+
+}
+function smaller(zat){
+    zat.style.fontSize = "25px";
+}
+function yesclick(element){
+    $('#yesterday').css('color', 'brown');
+    $('#today').css('color', 'white');
+    $('#oldest').css('color', 'white');
+
+}
