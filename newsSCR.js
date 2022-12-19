6e0fd18a806c4496e7bea76bb5f358a47e598b77
nuurlybekuly <210103269@stu.sdu.edu.kz>
2022-12-19 10:28:46 +0600

    gl

--- newsSCR.js
+++ newsSCR.js
@@ -7,28 +7,74 @@ function smaller(zat){
     zat.style.fontSize = "25px";
 }
 function yesclick(element){
+    $("#news1").hide(1000);
+    $("#news1_2").show(1000);
+    $("#news1_3").hide(1000);
+    $("#news1_4").hide(1000);
+
+
+    $('#news1_2').css('visibility', 'visible');
+    $('#sozlar1_2').css('margin-left', '30%');
+    $('#news1_2').css('display', 'flex');
+
+
+
+    $("#news2").hide(1000);
+    $("#news2_2").show(1000);
+    $("#news2_3").hide(1000);
+    $("#news2_4").hide(1000);
+
+    $('#news2_2').css('visibility', 'visible');
+    $('#sozlar2_2').css('margin-left', '30%');
+    $('#news2_2').css('display', 'flex');
+
+    $("#news3").hide(1000);
+    $("#news3_2").show(1000);
+    $("#news3_3").hide(1000);
+    $("#news3_4").hide(1000);
+
+    $('#news3_2').css('visibility', 'visible');
+    $('#sozlar3_2').css('margin-left', '30%');
+    $('#news3_2').css('display', 'flex');
+
+    $("#news4").hide(1000);
+    $("#news4_2").show(1000);
+    $("#news4_3").hide(1000);
+    $("#news4_4").hide(1000);
+
+    $('#news4_2').css('visibility', 'visible');
+    $('#news4_2').css('display', 'flex');
+
+    $("#news5").hide(1000);
+    $("#news5_2").show(1000);
+    $("#news5_3").hide(1000);
+    $("#news5_4").hide(1000);
+
+    $('#news5_2').css('visibility', 'visible');
+    $('#news5_2').css('display', 'flex');
+
+
+
     $('#yesterday').css('color', 'brown');
     $('#today').css('color', 'white');
     $('#weekago').css('color', 'white');
     $('#oldest').css('color', 'white');
 
-    $('#news1').css('visibility', 'hidden');
-    $('#news1').css('display', 'none');
-    $('#news1_2').css('visibility', 'visible');
-    $('#sozlar1_2').css('margin-left', '30%');
-    $('#news1_2').css('display', 'flex');
+    // $('#news1').css('visibility', 'hidden');
+    // $('#news1').css('display', 'none');
+    
 
-    $('#news2').css('visibility', 'hidden');
-    $('#news2').css('display', 'none');
-    $('#news2_2').css('visibility', 'visible');
-    $('#sozlar2_2').css('margin-left', '30%');
-    $('#news2_2').css('display', 'flex');
+    // $('#news2').css('visibility', 'hidden');
+    // $('#news2').css('display', 'none');
+    // $('#news2_2').css('visibility', 'visible');
+    // $('#sozlar2_2').css('margin-left', '30%');
+    // $('#news2_2').css('display', 'flex');
 
-    $('#news3').css('visibility', 'hidden');
-    $('#news3').css('display', 'none');
-    $('#news3_2').css('visibility', 'visible');
-    $('#sozlar3_2').css('margin-left', '30%');
-    $('#news3_2').css('display', 'flex');
+    // $('#news3').css('visibility', 'hidden');
+    // $('#news3').css('display', 'none');
+    // $('#news3_2').css('visibility', 'visible');
+    // $('#sozlar3_2').css('margin-left', '30%');
+    // $('#news3_2').css('display', 'flex');
 
 }
 function todayclick(element){
@@ -37,22 +83,142 @@ function todayclick(element){
     $('#weekago').css('color', 'white');
     $('#oldest').css('color', 'white');
 
-    $('#news1').css('visibility', 'visible');
-    $('#news1').css('display', 'flex');
-    $('#news1_2').css('visibility', 'hidden');
-    $('#sozlar1_2').css('margin-left', '30%');
-    $('#brr').css('display', 'none');
-    $('#news1_2').css('display', 'none');
+    $("#news1").show(1000);
+    $("#news1_2").hide(1000);
+    $("#news1_3").hide(1000);
+    $("#news1_4").hide(1000);
 
-    $('#news2').css('visibility', 'visible');
-    $('#news2').css('display', 'flex');
-    $('#news2_2').css('visibility', 'hidden');
-    $('#sozlar2_2').css('margin-left', '30%');
-    $('#news2_2').css('display', 'none');
+    $("#news2").show(1000);
+    $("#news2_2").hide(1000);
+    $("#news2_3").hide(1000);
+    $("#news2_4").hide(1000);
 
-    $('#news3').css('visibility', 'visible');
-    $('#news3').css('display', 'flex');
-    $('#news3_2').css('visibility', 'hidden');
-    $('#sozlar3_2').css('margin-left', '30%');
-    $('#news3_2').css('display', 'none');
+    $("#news3").show(1000);
+    $("#news3_2").hide(1000);
+    $("#news3_3").hide(1000);
+    $("#news3_4").hide(1000);
+
+    $("#news4").show(1000);
+    $("#news4_2").hide(1000);
+    $("#news4_3").hide(1000);
+    $("#news4_4").hide(1000);
+
+    $("#news5").show(1000);
+    $("#news5_2").hide(1000);
+    $("#news5_3").hide(1000);
+    $("#news5_4").hide(1000);
+
+}
+
+function weekclick(){
+    $('#yesterday').css('color', 'white');
+    $('#today').css('color', 'white');
+    $('#weekago').css('color', 'brown');
+    $('#oldest').css('color', 'white');
+
+    $("#news1").hide(1000);
+    $("#news1_2").hide(1000);
+    $("#news1_3").show(1000);
+    $("#news1_4").hide(1000);
+
+    $("#news2").hide(1000);
+    $("#news2_2").hide(1000);
+    $("#news2_3").show(1000);
+    $("#news2_4").hide(1000);
+
+    $("#news3").hide(1000);
+    $("#news3_2").hide(1000);
+    $("#news3_3").show(1000);
+    $("#news3_4").hide(1000);
+
+    $("#news4").hide(1000);
+    $("#news4_2").hide(1000);
+    $("#news4_3").show(1000);
+    $("#news4_4").hide(1000);
+
+    $("#news5").hide(1000);
+    $("#news5_2").hide(1000);
+    $("#news5_3").show(1000);
+    $("#news5_4").hide(1000);
+
+
+    $('#news1_3').css('visibility', 'visible');
+    $('#news1_3').css('display', 'flex');
+
+    $('#news2_3').css('visibility', 'visible');
+    $('#news2_3').css('display', 'flex');
+
+    $('#news3_3').css('visibility', 'visible');
+    $('#news3_3').css('display', 'flex');
+
+    $('#news4_3').css('visibility', 'visible');
+    $('#news4_3').css('display', 'flex');
+
+    $('#news5_3').css('visibility', 'visible');
+    $('#news5_3').css('display', 'flex');
+
+}
+function oldestclick(){
+
+    $('#yesterday').css('color', 'white');
+    $('#today').css('color', 'white');
+    $('#weekago').css('color', 'white');
+    $('#oldest').css('color', 'brown');
+
+    $("#news1").hide(1000);
+    $("#news1_2").hide(1000);
+    $("#news1_3").hide(1000);
+    $("#news1_4").show(1000);
+
+    $("#news2").hide(1000);
+    $("#news2_2").hide(1000);
+    $("#news2_3").hide(1000);
+    $("#news2_4").show(1000);
+
+    $("#news3").hide(1000);
+    $("#news3_2").hide(1000);
+    $("#news3_3").hide(1000);
+    $("#news3_4").show(1000);
+
+    $("#news4").hide(1000);
+    $("#news4_2").hide(1000);
+    $("#news4_3").hide(1000);
+    $("#news4_4").show(1000);
+
+    $("#news5").hide(1000);
+    $("#news5_2").hide(1000);
+    $("#news5_3").hide(1000);
+    $("#news5_4").show(1000);
+
+
+    $('#news1_4').css('visibility', 'visible');
+    $('#news1_4').css('display', 'flex');
+
+    $('#news2_4').css('visibility', 'visible');
+    $('#news2_4').css('display', 'flex');
+
+    $('#news3_4').css('visibility', 'visible');
+    $('#news3_4').css('display', 'flex');
+
+    $('#news4_4').css('visibility', 'visible');
+    $('#news4_4').css('display', 'flex');
+
+    $('#news5_4').css('visibility', 'visible');
+    $('#news5_4').css('display', 'flex');
+
+}
+function youtube(){
+    window.location.href ="https://youtube.com/@marvel";
+}
+function telegram(){
+    window.location.href ="http://127.0.0.1:5500/laslab4.html";
+}
+function insta(){
+    window.location.href ="https://t.me/MarvelEntertainment_1";
+}
+function facebook(){
+    window.location.href ="http://127.0.0.1:5500/laslab4.html";
+}
+function twitter(){
+    window.location.href ="http://127.0.0.1:5500/laslab4.html";
 }
