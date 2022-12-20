8fd0f1591c0de06288a168e1cf0e7e6bcd5f8974
bake08 <210103194@stu.sdu.edu.kz>
2022-12-20 13:46:42 +0600

    Osylai go

--- Register.php
+++ Register.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 $email=$_POST['email'];
 $password=$_POST['password'];
@@ -23,30 +23,4 @@ if($conn->connect_error){
 setTimeout('window.location="oop.html"',1);
 </script>
     </head>
-=======
-<?php
-$email=$_POST['email'];
-$password=$_POST['password'];
-$conn = new mysqli('localhost','root','','final');
-if($conn->connect_error){
-    die('COnnection Failed : '.$conn->connect_error);
-}else{
-    
-    $stmt=$conn->prepare("insert into accounts(email,password) values(?,?)");
-    $stmt->bind_param("ss",$email,$password,);
-    $stmt->execute();
-    
-    $stmt->close();
-    $conn->close();
-}
-
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-    <script type="text/javascript">
-setTimeout('window.location="oop.html"',1);
-</script>
-    </head>
->>>>>>> c7bb0a944f9707dc0a06d98fe0fe62a75c4dbf58
 </html>
